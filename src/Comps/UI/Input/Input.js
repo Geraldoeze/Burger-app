5c15a8c379edd309386a3e3b1e92de4caf8ff4ac
Geraldoeze <egerald344@gmail.com>
2021-11-19 00:37:30 +0100

    input

---
+++ src/Comps/UI/Input/Input.js
@@ -0,0 +1,14 @@
+
+
+
+const input = (props) => {
+    
+    return ( 
+        <div>
+            <label>{props.labal}</label>
+            <input  />
+        </div>
+     );
+}
+ 
+export default input;
